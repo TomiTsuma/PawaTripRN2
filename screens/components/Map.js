662c851c42a534809a550bc8d77361ee23513df1
Thomas Tsuma <34827918+TomiTsuma@users.noreply.github.com>
2022-01-03 16:41:34 -0800

    get location

--- screens/components/Map.js
+++ screens/components/Map.js
@@ -3,7 +3,7 @@ import { StyleSheet} from 'react-native'
 import { useSelector } from 'react-redux'
 import tw from 'tailwind-react-native-classnames'
 import { selectOrigin } from '../../slices/navSlice'
-import GetLocation from 'react-native-get-location'
+//import GetLocation from 'react-native-get-location'
 import MapboxGL from '@react-native-mapbox-gl/maps';
 
 
@@ -16,12 +16,12 @@ const Map = () => {
 <MapboxGL.MapView
     style={tw`flex-1 `}
     // mapType="mutedStandard"
-    // initialRegion={{
-    //    latitude: 36.7685,
-    //    longitude: 1.3823,
-    //   latitudeDelta: 0.0922,
-    //   longitudeDelta: 0.0421,
-    // }}
+    initialRegion={{
+       latitude: 36.7685,
+       longitude: 1.3823,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }}
   />    
     )
 }
